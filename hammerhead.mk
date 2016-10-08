6f080666703bcde34f9e475421ad55aa0c1165fc
Ruchi Kandoi <kandoiruchi@google.com>
2016-10-07 19:29:28 -0700

    hammerhead: Make powerHAL compatible for passthrough HIDL design
    Bug: 31177288
    Change-Id: Ia65f6b50d091a02199565af1d8855ec7473e7323
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- hammerhead.mk
+++ hammerhead.mk
@@ -210,8 +210,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    power.hammerhead \
-    power.msm8974
+    power.hammerhead
 
 # Ramdisk
 PRODUCT_PACKAGES += \
