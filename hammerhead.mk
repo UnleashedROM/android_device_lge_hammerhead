91e7f63950083d2d17d453c6ed99f24970ddd81e
Mikhail Naganov <mnaganov@google.com>
2016-11-21 15:22:17 -0800

    hammerhead: Add all audio HIDL *-impl libraries to the list of product packages
    As *-impl.so files are loaded via dlopen, they don't get added
    automatically with the audio HAL server.
    
    Test: check files on the device
    Change-Id: I9ec8e0b7883d88f3077fcb425a33f19162b2f1df
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- hammerhead.mk
+++ hammerhead.mk
@@ -24,6 +24,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audio.primary.msm8974 \
     audio.a2dp.default \
     audio.usb.default \
