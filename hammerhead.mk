6dafc12bf7bd6716c37d63ae722040d597b840e4
Steven Moreland <smoreland@google.com>
2017-01-06 14:12:08 -0800

    hammerhead: Add memtrack HAL
    Fix: 34103653
    Test: `adb logcat | grep memtrack` should produce no messages like:
    memtrack: Couldn't load memtrack module
    Change-Id: Ie1b1e18a2764b919c0f86d292039369a4d6a8146
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- hammerhead.mk
+++ hammerhead.mk
@@ -148,6 +148,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
 # Misc
 PRODUCT_CHARACTERISTICS := nosdcard
 
