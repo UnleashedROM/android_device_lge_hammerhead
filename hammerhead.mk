1e9f463885fc1778d56732b99a4502a998a9bc58
Eino-Ville Talvala <etalvala@google.com>
2017-01-19 16:12:42 -0800

    hammerhead: Add necessary camera HAL libraries for passthrough HIDL operation
    Test: No regressions in a full camera CTS pass w/HIDL enabled
          in camara service.
    Bug: 30985004
    Change-Id: Ie7206c322db893eacab0ba8e6647c1587fccca42
    
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- hammerhead.mk
+++ hammerhead.mk
@@ -51,6 +51,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     libqomx_core \
